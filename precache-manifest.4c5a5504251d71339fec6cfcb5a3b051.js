4514222150c40f7616d566591e96d9ab9bd81b62
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-05-04 23:55:05 +0300

    Initial dist subtree commit

---
+++ precache-manifest.4c5a5504251d71339fec6cfcb5a3b051.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac438482f3282fd21b75",
+    "url": "/Vue-CRM/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "ac05519166b3351122b4",
+    "url": "/Vue-CRM/css/chunk-72aa6c85.fcd7331c.css"
+  },
+  {
+    "revision": "8c6d9cb8809fb2c63cd7",
+    "url": "/Vue-CRM/css/chunk-vendors.3b8adcd8.css"
+  },
+  {
+    "revision": "3380a225d0440401ed958110e9da39bd",
+    "url": "/Vue-CRM/index.html"
+  },
+  {
+    "revision": "ac438482f3282fd21b75",
+    "url": "/Vue-CRM/js/app.fdddcb38.js"
+  },
+  {
+    "revision": "bda02ab58564e074918e",
+    "url": "/Vue-CRM/js/chunk-10b86dd9.96687baf.js"
+  },
+  {
+    "revision": "a11d635677ffb234509f",
+    "url": "/Vue-CRM/js/chunk-2d217a99.a4727597.js"
+  },
+  {
+    "revision": "d5253892dd998c9a5b11",
+    "url": "/Vue-CRM/js/chunk-2d21a3d2.bcc5e859.js"
+  },
+  {
+    "revision": "98f66922e1e306c6f7e0",
+    "url": "/Vue-CRM/js/chunk-49ee4951.751c2bdc.js"
+  },
+  {
+    "revision": "46f5e6933dc0d53be877",
+    "url": "/Vue-CRM/js/chunk-53c8ec65.4184822d.js"
+  },
+  {
+    "revision": "7ab74413e1e360777412",
+    "url": "/Vue-CRM/js/chunk-5f4ad635.f9abc7b7.js"
+  },
+  {
+    "revision": "2162878dd98ac8fef4b2",
+    "url": "/Vue-CRM/js/chunk-72449522.8bd088a2.js"
+  },
+  {
+    "revision": "ac05519166b3351122b4",
+    "url": "/Vue-CRM/js/chunk-72aa6c85.4bc8d8a2.js"
+  },
+  {
+    "revision": "991a3bab42fa41dbe9ff",
+    "url": "/Vue-CRM/js/chunk-c420df12.7429e3d3.js"
+  },
+  {
+    "revision": "8c6d9cb8809fb2c63cd7",
+    "url": "/Vue-CRM/js/chunk-vendors.a022c8ec.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/Vue-CRM/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-CRM/robots.txt"
+  }
+]);
